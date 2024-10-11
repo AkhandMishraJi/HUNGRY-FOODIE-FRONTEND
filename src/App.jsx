339793bd616af94382b43a2418ef5e89db6f58c2
AKHAND PRATAP MISHRA <kanhamishra286@gmail.com>
2024-10-11 12:14:29 +0530

    Added Add Product Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from './Pages/Home'
 import SignUp from './Pages/Auth/Signup'
 import Login from './Pages/Auth/Login'
 import NotFound from './Pages/NotFound'
+import Denied from './Pages/Auth/Denied'
+import AddProduct from './Pages/Admin/AddProduct'
 
 function App() {
 
@@ -14,9 +16,10 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/denied' element={<Denied/>} />
       <Route path='/auth/signup' element={<SignUp/>} />
       <Route path='/auth/login' element={<Login/>} />
-      
+      <Route path='/admin/addProduct' element={<AddProduct/>} />
       <Route path='*' element={<NotFound />} />
     </Routes>
     </>
---
+++ src/Pages/Admin/AddProduct.jsx
@@ -0,0 +1,98 @@
+import Layout from "../../Layouts/Layout"
+
+function AddProduct() {
+    return (
+        <Layout>
+            <section className="py-12">
+                <div className="max-w-md mx-auto mt-8 bg-white p-7">            
+                    <h2 className="mb-4 text-2xl font-semibold">Add Product</h2>
+
+                    <form action="">
+
+                        <div className="mb-4">
+                            <label htmlFor="productImage" className="block text-sm font-medium text-gray-700">Product Image <span className="text-red-500 text-[12px]">(Only .jpg , .jpeg , .png , .gif Accepted)</span></label>
+                            <input type="file" 
+                            accept=".jpg , .jpeg , .png , .gif"
+                            name="productImage"
+                            id="productImage"
+                            className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            />
+                        </div>
+
+                        <div className="mb-4">
+                            <label htmlFor="productName" className="block text-sm font-medium text-gray-700">
+                              Product Name <span className="text-red-500">*</span>
+                            </label>
+                            <input type="text" 
+                            name="productName" 
+                            required
+                            minLength={2}
+                            id="productName"
+                            className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            />
+                        </div>
+
+                        <div className="mb-4">
+                            <label htmlFor="productDescription" className="block text-sm font-medium text-gray-700">
+                              Product Description<span className="text-red-500">*</span>
+                            </label>
+                            <input type="text" 
+                            name="productDescription" 
+                            minLength={10}
+                            id="productDescription"
+                            className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            />
+                        </div>
+
+                        <div className="mb-4">
+                            <label htmlFor="productPrice" className="block text-sm font-medium text-gray-700">
+                              Product Price <span className="text-red-500">*</span>
+                            </label>
+                            <input type="number" 
+                            name="productPrice" 
+                            required
+                            id="productPrice"
+                            className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            />
+                        </div>
+
+                        <div className="mb-4">
+                            <label htmlFor="productQuantity" className="block text-sm font-medium text-gray-700">
+                              Product Quantity <span className="text-red-500">*</span>
+                            </label>
+                            <input type="number" 
+                            name="productQuantity" 
+                            required
+                            id="productQuantity"
+                            className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            />
+                        </div>
+
+                        <div className="mb-2">
+                            <label htmlFor="productCategory">Product Category <span className="text-red-500">*</span></label>
+                            <select name="productCategory" id="productCategory" className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <option value="veg">Veg</option>
+                                <option value="non-veg">Non-veg</option>
+                                <option value="drinks">Drinks</option>
+                                <option value="sides">Sides</option>
+                                <option value="food">Food</option>
+                                <option value="pizza">Pizza</option>
+                            </select>
+                        </div>
+                        
+                        <button
+                        type="submit"
+                        className="w-full bg-yellow-500 text-white p-2 rounded-md hover:bg-yellow-600 transition duration-300 ease-in-out"
+                        >
+                            Add Product
+                        </button>
+
+                    </form>
+
+                </div>
+            </section>   
+        </Layout>
+    )
+}
+
+export default AddProduct
